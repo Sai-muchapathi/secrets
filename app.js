d6cd20d8a84b0071b1642943488136cf35304cd7
sai kiran muchapathi <saim80191@gmail.com>
2023-09-19 17:43:54 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,74 @@
+//jshint esversion:6
+const express = require("express");
+const ejs = require("ejs");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const app = express();
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+const port = 3000;
+
+mongoose.connect("mongodb://127.0.0.1:27017/userDB", {useNewUrlParser:true});
+
+const userSchema = {
+    email: String,
+    password: String
+};
+
+const User = new mongoose.model("User", userSchema);
+
+
+
+app.get("/", (req, res) => {
+    res.render("home");
+});
+
+app.get("/login", (req, res) => {
+    res.render("login");
+});
+
+app.get("/register", (req, res) => {
+    res.render("register");
+});
+
+app.post("/register", function(req, res) {
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });  
+    newUser.save(function(err) {
+        if(err) {
+            console.log(err);
+        } else {
+            res.render("secrets");
+        }
+    });  
+});
+
+app.post("/login", function(req, res) {
+    const userName = req.body.username;
+    const passWord = req.body.password;
+    User.findOne({email: userName}, function(err, foundUser) {
+        if(err) {
+            
+            console.log(err);
+            
+        } else {
+            if(foundUser) {
+                if(foundUser.password === passWord) {
+                    res.render("secrets");
+                }
+            }
+        }
+    });
+});
+
+app.get("/logout", (req, res) => {
+    res.redirect("/");
+});
+
+app.listen(port, () => {
+    console.log("Listening on port 3000......");
+});
