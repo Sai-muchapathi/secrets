d67e541377bdf4a6bd6de65221a8bd7ccc557809
sai kiran muchapathi <saim80191@gmail.com>
2023-09-19 18:13:08 -0500

    hashing passwords

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 
 
@@ -41,7 +42,7 @@ app.get("/register", (req, res) => {
 app.post("/register", function(req, res) {
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });  
     newUser.save(function(err) {
         if(err) {
@@ -54,7 +55,7 @@ app.post("/register", function(req, res) {
 
 app.post("/login", function(req, res) {
     const userName = req.body.username;
-    const passWord = req.body.password;
+    const passWord = md5(req.body.password);
     User.findOne({email: userName}, function(err, foundUser) {
         if(err) {
             
